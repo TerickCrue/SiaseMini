100747579bc36d980ae51e5c7d1d8ffe97067397
Erick <118601533+TerickCrue@users.noreply.github.com>
2025-05-23 00:38:10 -0600

    change(component): home / dashboard
    added menu bar with items.
    modified routing to use menubar

--- Frontend/siase-mini-web/src/app/pages/home/home.component.ts
+++ Frontend/siase-mini-web/src/app/pages/home/home.component.ts
@@ -1,13 +1,41 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+import { MenubarModule } from 'primeng/menubar';
+import { MenuItem } from 'primeng/api';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, RouterModule, MenubarModule],
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent {
-
+  menuItems: MenuItem[] = [
+    {
+      label: 'Inicio',
+      routerLink: '/home/dashboard'
+    },
+    {
+      label: 'Carreras',
+      routerLink: '/home/careers'
+    },
+    {
+      label: 'Horarios',
+      routerLink: '/home/schedules'
+    },
+    {
+      label: 'Kardex',
+      routerLink: '/home/kardex'
+    },
+    {
+      label: 'Calificaciones',
+      routerLink: '/home/grades'
+    },
+    {
+      label: 'AFIs',
+      routerLink: '/home/afis'
+    }
+  ];
 }
--- Frontend/siase-mini-web/src/app/pages/home/home.routes.ts
+++ Frontend/siase-mini-web/src/app/pages/home/home.routes.ts
@@ -7,8 +7,33 @@ export default [
     component: HomeComponent,
     children: [
       {
+        path: '',
+        redirectTo: 'dashboard',
+        pathMatch: 'full'
+      },
+      {
         path: 'dashboard',
         loadComponent: () => import('./dashboard/dashboard.component').then((c) => c.DashboardComponent),
+      },
+      {
+        path: 'careers',
+        loadComponent: () => import('./careers/careers.component').then((c) => c.CareersComponent),
+      },
+      {
+        path: 'schedules',
+        loadComponent: () => import('./schedules/schedules.component').then((c) => c.SchedulesComponent),
+      },
+      {
+        path: 'kardex',
+        loadComponent: () => import('./kardex/kardex.component').then((c) => c.KardexComponent),
+      },
+      {
+        path: 'grades',
+        loadComponent: () => import('./grades/grades.component').then((c) => c.GradesComponent),
+      },
+      {
+        path: 'afis',
+        loadComponent: () => import('./afis/afis.component').then((c) => c.AfisComponent),
       },
       {
         path: '**',
