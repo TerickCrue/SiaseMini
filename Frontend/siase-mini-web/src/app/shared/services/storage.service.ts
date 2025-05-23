db59d876811610455fa291f54319c8638a35347d
Erick <118601533+TerickCrue@users.noreply.github.com>
2025-05-22 18:14:31 -0600

    create(service): storageService

---
+++ Frontend/siase-mini-web/src/app/shared/services/storage.service.ts
@@ -0,0 +1,35 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class StorageService {
+
+  constructor() { }
+
+  // Guardar un valor (como string o lo convierte si es objeto)
+  setItem(key: string, value: any): void {
+    const data = typeof value === 'string' ? value : JSON.stringify(value);
+    localStorage.setItem(key, data);
+  }
+
+  // Obtener un valor como string o parseado si es JSON
+  getItem<T = any>(key: string): T | null {
+    const item = localStorage.getItem(key);
+    try {
+      return item ? JSON.parse(item) : null;
+    } catch {
+      return item as T;
+    }
+  }
+
+  // Eliminar un valor
+  removeItem(key: string): void {
+    localStorage.removeItem(key);
+  }
+
+  // Limpiar todo el localStorage
+  clear(): void {
+    localStorage.clear();
+  }
+}
