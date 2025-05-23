ca1aba73dc33b31ae71add06ff38b5f231a9c826
Erick <118601533+TerickCrue@users.noreply.github.com>
2025-05-22 18:14:15 -0600

    config(primeng): providers

--- Frontend/siase-mini-web/src/app/app.config.ts
+++ Frontend/siase-mini-web/src/app/app.config.ts
@@ -1,8 +1,27 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
-
 import { routes } from './app.routes';
+import { provideHttpClient, withInterceptors } from '@angular/common/http';
+import { TokenInterceptor } from './shared/auth/func-token.interceptor';
+import { providePrimeNG } from 'primeng/config';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import Lara from '@primeng/themes/lara';
+
+import { DialogService } from 'primeng/dynamicdialog';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }), 
+    provideRouter(routes),
+    provideHttpClient(
+      withInterceptors([TokenInterceptor])
+    ),
+    provideAnimationsAsync(),
+    providePrimeNG({
+      theme: {
+        preset: Lara
+      }
+    }),
+    DialogService,
+  ]
 };
