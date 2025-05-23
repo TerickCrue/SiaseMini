04925e32f0d2f1d06742da7a8a87a6640f762b8d
Erick <118601533+TerickCrue@users.noreply.github.com>
2025-05-23 00:34:54 -0600

    create(components): nav components

---
+++ Frontend/siase-mini-web/src/app/pages/home/afis/afis.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-afis',
+  imports: [],
+  templateUrl: './afis.component.html',
+  styleUrl: './afis.component.scss'
+})
+export class AfisComponent {
+
+}
---
+++ Frontend/siase-mini-web/src/app/pages/home/kardex/kardex.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-kardex',
+  imports: [],
+  templateUrl: './kardex.component.html',
+  styleUrl: './kardex.component.scss'
+})
+export class KardexComponent {
+
+}
