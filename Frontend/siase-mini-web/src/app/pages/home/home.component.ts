71fd7da381f6c247de3e124b0cd3468d3ad1fc0a
Erick <118601533+TerickCrue@users.noreply.github.com>
2025-05-23 20:44:31 -0600

    change(component): menubar
     take menubar to separate component

--- Frontend/siase-mini-web/src/app/pages/home/home.component.ts
+++ Frontend/siase-mini-web/src/app/pages/home/home.component.ts
@@ -1,69 +1,22 @@
-import { AfterViewInit, Component, OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { Router, RouterModule } from '@angular/router';
-import { MenubarModule } from 'primeng/menubar';
-import { MenuItem } from 'primeng/api';
-import { ButtonModule } from 'primeng/button';
-import { AvatarModule } from 'primeng/avatar';
-import { StorageService } from '../../shared/services/storage.service';
-import { AuthService } from '../../shared/services/auth.service';
-import { GeneralConstant } from '../../shared/general-constant';
-import { LoginResponse } from '../../shared/dto/login/login-response.interface';
+import { RouterModule } from '@angular/router';
+import { HeaderMenuBarComponent } from "../../shared/components/layout/header-menu-bar/header-menu-bar.component";
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [CommonModule, RouterModule, MenubarModule, ButtonModule, AvatarModule],
+  imports: [CommonModule, RouterModule, HeaderMenuBarComponent],
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
 
-  menuItems: MenuItem[] = [
-    {
-      label: 'Inicio',
-      routerLink: '/home/dashboard'
-    },
-    {
-      label: 'Carreras',
-      routerLink: '/home/careers'
-    },
-    {
-      label: 'Horarios',
-      routerLink: '/home/schedules'
-    },
-    {
-      label: 'Kardex',
-      routerLink: '/home/kardex'
-    },
-    {
-      label: 'Calificaciones',
-      routerLink: '/home/grades'
-    },
-    {
-      label: 'AFIs',
-      routerLink: '/home/afis'
-    }
-  ];
-
-  private avatarPlaceholder = "/user-avatar-placeholder.svg"
-  protected userImg: string = "";
-
-  constructor(
-    private storageService: StorageService,
-    private authService: AuthService
-  ) {}
+  constructor(){
+    
+  }
 
   ngOnInit(): void {
-    this.userImg = this.getUserImg();
   }
 
-  protected logout() {
-    this.authService.logout();
-  }
-
-  private getUserImg(){
-    const userData = this.storageService.getItem(GeneralConstant.USER_DATA_KEY) as LoginResponse;
-    return  userData?.foto ?? this.avatarPlaceholder;
-  }
 }
---
+++ Frontend/siase-mini-web/src/app/shared/components/layout/header-menu-bar/header-menu-bar.component.ts
@@ -0,0 +1,118 @@
+import { Component } from '@angular/core';
+import { MenuItem } from 'primeng/api';
+import { StorageService } from '../../../services/storage.service';
+import { AuthService } from '../../../services/auth.service';
+import { GeneralConstant } from '../../../general-constant';
+import { LoginResponse } from '../../../dto/login/login-response.interface';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+import { MenubarModule } from 'primeng/menubar';
+import { ButtonModule } from 'primeng/button';
+import { AvatarModule } from 'primeng/avatar';
+import { PopoverModule } from 'primeng/popover';
+import { SelectModule } from 'primeng/select';
+import { FormsModule } from '@angular/forms';
+import { Career } from '../../../dto/career/career.interface';
+import { DrawerModule } from 'primeng/drawer';
+
+@Component({
+  selector: 'app-header-menu-bar',
+  imports: [
+    CommonModule,
+    FormsModule,
+    RouterModule,
+    MenubarModule,
+    ButtonModule,
+    AvatarModule,
+    PopoverModule,
+    SelectModule,
+    DrawerModule
+  ],
+  templateUrl: './header-menu-bar.component.html',
+  styleUrl: './header-menu-bar.component.scss',
+})
+export class HeaderMenuBarComponent {
+  menuItems: MenuItem[] = [
+    {
+      label: 'Inicio',
+      routerLink: '/home/dashboard',
+    },
+    {
+      label: 'Carreras',
+      routerLink: '/home/careers',
+    },
+    {
+      label: 'Horarios',
+      routerLink: '/home/schedules',
+    },
+    {
+      label: 'Kardex',
+      routerLink: '/home/kardex',
+    },
+    {
+      label: 'Calificaciones',
+      routerLink: '/home/grades',
+    },
+    {
+      label: 'AFIs',
+      items: [
+        {
+          label: 'Calendario',
+          routerLink: '/home/afis/calendar'
+        },
+        {
+          label: 'Historial',
+          routerLink: '/home/afis/history'
+        }
+      ]
+    },
+  ];
+
+  protected userData!: LoginResponse;
+  protected selectedCareer!: Career;
+  protected isSettingsVisible: boolean = false;
+
+  constructor(
+    private storageService: StorageService,
+    private authService: AuthService
+  ) {}
+
+  ngOnInit(): void {
+    this.userData = this.getUserData();
+    this.setUserCareer(this.userData);
+  }
+
+  protected logout() {
+    this.authService.logout();
+  }
+
+  private getUserData() {
+    const userData = this.storageService.getItem(
+      GeneralConstant.USER_DATA_KEY
+    ) as LoginResponse;
+
+    return userData;
+  }
+
+  protected toggleSettingsDrawer(){
+    this.isSettingsVisible = !this.isSettingsVisible;
+  }
+
+  protected setUserCareer(userData: LoginResponse){
+    const lastCareerIndex = this.storageService.getItem(GeneralConstant.USER_SELECTED_CAREER_KEY);
+
+    if(lastCareerIndex == null){
+      const index = (userData.carreras.length - 1);
+      this.selectedCareer = userData.carreras[index];
+      this.storageService.setItem(GeneralConstant.USER_SELECTED_CAREER_KEY, index);
+    }
+
+    this.selectedCareer = userData.carreras[lastCareerIndex];
+  }
+
+  protected onCareerChange(){
+    const careerIndex = this.userData.carreras.indexOf(this.selectedCareer);
+    this.storageService.setItem(GeneralConstant.USER_SELECTED_CAREER_KEY, careerIndex)
+    location.reload();
+  }
+}
