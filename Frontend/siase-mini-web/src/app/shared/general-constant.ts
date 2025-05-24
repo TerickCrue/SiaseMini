aeeeb150974b340186c4103b94f5866929347ed2
Erick <118601533+TerickCrue@users.noreply.github.com>
2025-05-23 20:38:59 -0600

    Update general-constant.ts

--- Frontend/siase-mini-web/src/app/shared/general-constant.ts
+++ Frontend/siase-mini-web/src/app/shared/general-constant.ts
@@ -1,4 +1,6 @@
 export class GeneralConstant { 
     public static TOKEN_KEY = 'user-token';
     public static USER_DATA_KEY = 'user-data';
+    public static USER_SELECTED_CAREER_KEY = 'user-selected-career';
+
 }
