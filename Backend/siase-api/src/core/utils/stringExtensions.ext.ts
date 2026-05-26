29e63a05fb050a32230b5437ff32d867109fc3e7
Erick <118601533+TerickCrue@users.noreply.github.com>
2026-05-25 19:08:40 -0600

    Revert "Create Backend"
    This reverts commit aa73f4c60edae808129933361c4161886962c266.

--- Backend/siase-api/src/core/utils/stringExtensions.ext.ts
+++
@@ -1,8 +0,0 @@
-
-interface String {
-    capitalizeFirst(): string;
-}
-
-String.prototype.capitalizeFirst = function (this: string): string {
-    return this.charAt(0).toUpperCase() + this.slice(1).toLowerCase();
-}
--- Backend/siase-api/src/webapi/models/models.ts
+++
@@ -1,8 +0,0 @@
-export interface TokenPayload {
-    user: string
-    trim: string
-    careers: object[]
-    picture: string
-    name: string
-}
-
